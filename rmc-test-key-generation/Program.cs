4faca1cc5255b185dd78787381baaeb4213df52d
CryptoManiac <balthazar@yandex.ru>
2018-03-09 20:38:57 +0300

    Key generation test

---
+++ rmc-test-key-generation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RMC.Signing;
+
+namespace rmc_test_key_generation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seed = Seed.FromRandom();
+
+            Console.WriteLine("Random seed: {0}", seed.ToString());
+
+            for (var index = 0; index < 10; index++)
+            {
+                var keyPair = seed.KeyPair(index);
+                Console.WriteLine("Key #{0} : ({1}, {2})", index, keyPair.Id(), keyPair.ToString());
+            }
+        }
+    }
+}
