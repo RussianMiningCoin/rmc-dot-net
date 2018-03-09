18da54e34f41789bd784a6d1edb0ca49f21fc980
CryptoManiac <balthazar@yandex.ru>
2018-03-09 19:47:08 +0300

    Add simple testing program

---
+++ rmc-test-signing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using RMC.Address;
+using RMC.Core;
+using RMC.Signing;
+using RMC.TxSigning;
+using Newtonsoft.Json.Linq;
+
+namespace rmc_test_signing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = "35tDMF67PWq8XmqY88CjBPZspfwX2"; // HisDivineShadow
+            var unsignedTxJson = @"{
+                'Account': '19HVVcvqHvFocvwpdN4nB5AXXgg19qqpw3',
+                'Amount': '1000',
+                'Destination': '15ipnwHm6VV8TR8bpbkG5zo4FbgT5ZqrFq',
+                'Fee': '10',
+                'Flags': 2147483648,
+                'Sequence': 1,
+                'TransactionType' : 'Payment'
+            }";
+
+            var signed = TxSigner.SignJson(JObject.Parse(unsignedTxJson), secret);
+
+            Console.WriteLine(signed.Hash);
+            Console.WriteLine(signed.TxJson);
+            Console.WriteLine(signed.TxBlob);
+
+        }
+    }
+}
